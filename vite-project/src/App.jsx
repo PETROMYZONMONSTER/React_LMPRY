6b086101594393b0979bb90e7e3f84c3831f852b
Petromyzonmonster <petromyzontiform@gmail.com>
2024-10-10 19:03:34 -0300

    React class atv2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,27 +3,42 @@ import './App.css'
 function App() {
 
   return (
-    <main>
-        <div className="general">
-            <header>
-                <h2>Monitoria React</h2>
-            </header>
-            <div className="container">
-                <h1>Login</h1>
-                <label>
-                    <h4>Username</h4>
-                    <input type="text"
-                           name="UserName"/>
-                </label>
-                <label>
-                    <h4>Password</h4>
-                    <input type="password"
-                           name="PassWord"/>
-                </label>
-                <button className="submit">Enter</button>
-            </div>
-        </div>
-    </main>
+      <main>
+          <div className="general">
+              <header>
+                  <h2>Monitoria React</h2>
+              </header>
+              <h1>Register</h1>
+                <div className="container-division">
+                  <div className="container">
+                      <label>
+                          <h4>Username</h4>
+                          <input type="text"
+                                 name="UserName"/>
+                      </label>
+                      <label>
+                          <h4>Password</h4>
+                          <input type="password"
+                                 name="PassWord"/>
+                      </label>
+                  </div>
+                    <div className="vertical-row"></div>
+                  <div className="container">
+                      <label>
+                          <h4>Username</h4>
+                          <input type="text"
+                                 name="UserName"/>
+                      </label>
+                      <label>
+                          <h4>Password</h4>
+                          <input type="password"
+                                 name="PassWord"/>
+                      </label>
+                  </div>
+                </div>
+              <button className="submit">Enter</button>
+          </div>
+      </main>
   )
 }
 
